4551b4efdffebf2a98813900e8ed9ad200f44e9a
SnehaSoom <soomsneha05@gmail.com>
2024-09-24 15:04:54 +0530

    CSS Fix

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,3 +1,7 @@
+<script setup>
+import '../assets/base.css'
+</script>
+
 <template>
   <footer class="bg-neutral-100 shadow bg-gray-800">
     <div class="max-w-screen-xl p-4 flex justify-between items-center">
@@ -16,31 +20,3 @@
   </footer>
 </template>
 
-<style>
-@media (min-width: 768px) {
-  .privacyList{
-    display: flex;
-  }
-}
-  /* Make the footer responsive */
-  @media (max-width: 768px) {
-    .flex {
-      flex-wrap: wrap;
-    }
-    .justify-between {
-      justify-content: center;
-    }
-  }
-  @media (max-width: 480px) {
-    .text-sm {
-      font-size: 0.8rem;
-    }
-    .gap-4 {
-      gap: 2rem;
-    }
-  }
-
-  .flex {
-    flex-wrap: nowrap;
-  }   
-</style>

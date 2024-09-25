33ed57bd979191a569049f5cd6d79f02d654511d
Vjy <vijay@iavian.com>
2024-09-25 18:05:40 +0530

    Add exif cleanup

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,27 +1,20 @@
-<script setup>
-import '../assets/base.css'
-</script>
-
 <template>
-  <footer class="text-center bg-white w-full border-t border-gray-300 py-1 md:h-11">
+  <footer class=" bg-neutral-200 text-center  w-full border-t border-gray-300 py-1 md:h-11">
     <div class="flex items-center justify-between text-center container mb-0 mx-auto max-w-5xl py-2 px-8">
-      <span class="text-xs text-neutral-400">
-        Copyright © 2024 <a href="https://iavian.com/" class="hover:underline">Vijay Anand.</a>
-      </span>  
-      <ul class="flex text-sm text-blue-900 gap-1">
-  <li class="whitespace-nowrap">
-    <a href="#" class="hover:underline">Terms</a>
-  </li>    
-  <li class="mx-1">|</li>    
-  <li>
-    <a href="#" class="hover:underline">Privacy</a>
-  </li>
-  <li class="mx-1">|</li>
-  <li>
-    <a href="#" class="hover:underline">Help</a>
-  </li>
-</ul>
+      <small class="text-neutral-800 ">Copyright © 2024 Vijay Anand.</small>
+      <small class="font-lato flex space-x-2 text-blue-600 dark:text-blue-900">
+        <RouterLink class="hover:underline" to="/terms">
+          Terms
+        </RouterLink>
+        <span class="text-neutral-900">|</span>
+        <RouterLink class="hover:underline" to="/privacy">
+          Privacy
+        </RouterLink>
+        <span class="text-neutral-900">|</span>
+        <RouterLink class="hover:underline" to="/help">
+          Help
+        </RouterLink>
+      </small>
     </div>
   </footer>
 </template>
-
--- src/components/Slug.vue
+++ src/components/Slug.vue
@@ -1,17 +1,16 @@
 <template>
   <div class="container mx-auto bg-fixed max-w-5xl py-1 px-8">
-    <h2 class="my-2 py-1 text-4xl font-bold leading-none sm:px-1 tracking-tight md:text-5xl lg:text-4xl">
-      <span class="text-primary font-bold" style="font-family: Montserrat">Privacy first, Secure, Offline </span>Multimedia file metadata tool
+    <h2 class="my-2 py-1 text-4xl font-extrabold leading-none sm:px-1 tracking-tight md:text-5xl lg:text-4xl">
+      <span class="emo">Privacy first, Secure, Offline </span>Multimedia file metadata tool
     </h2>
-    <p class="text-lg font-normal text-gray-500 lg:text-xl sm:px-1 dark:text-gray-400">
+    <p class="my-2 text-lg font-normal text-gray-500 lg:text-xl sm:px-1">
       Enhanced privacy, most secure and offline Multimedia file metadata tool.
       <b>Your file never leaves your device.</b>
     </p>
   </div>
 </template>
-
 <style scoped>
-.text-primary {
+.emo {
   color: rgb(16, 100, 164);
 }
 </style>

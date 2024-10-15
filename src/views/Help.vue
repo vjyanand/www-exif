8aca58ada1fe6131aef74ee77f4d38d236a6ca7b
Vjy <vijay@iavian.com>
2024-10-15 14:40:10 +0530

    Add exif cleanup

--- src/views/Help.vue
+++ src/views/Help.vue
@@ -6,21 +6,18 @@
     <p class="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">One of the latest advancements in this field is the use of WebAssembly (WASM) technology, which eliminates the need for an internet connection during the conversion process. <a href="https://pdf.iavian.net/blog/posts/wasm-advantage">Read more..</a></p>
 
     <h2 class="text-2xl font-medium dark:text-white">How secure is it?</h2>
-    <p class="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">When using a WASM-based offline PDF table converter, you can rest assured that your data is secure and protected. The converter operates locally on your device, ensuring that sensitive information does not leave your system. This eliminates the risk of data leakage and unauthorized access to your data.</p>
+    <p class="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">When using a WASM-based offline exif tool, you can rest assured that your data is secure and protected. The tool operates locally on your device, ensuring that sensitive information does not leave your system. This eliminates the risk of data leakage and unauthorized access to your data.</p>
 
-    <h2 class="text-2xl font-medium dark:text-white">What PDF files are supported?</h2>
-    <p class="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">Currently, only text based PDF tables are supported. Image based texts and tables are not supported</p>
+    <h2 class="text-2xl font-medium dark:text-white">What media files are supported?</h2>
+    <p class="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">Currently, JPG, GIF and PNG files are supported.</p>
 
-    <h2 class="text-2xl font-medium dark:text-white">Does it support all kinds of tables?</h2>
-    <p class="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">Our algorithm makes best guess to detect tables and their structure to parse. However, there are few tables that are nested and complex which we are working on.</p>
-
-    <h2 class="text-2xl font-medium dark:text-white">How accurate is the conversion?</h2>
-    <p class="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">We use specialized algorithms to accurately convert even the most complex tables into CSV format without losing any data or structure. However, ensure the accuracy and reliability of your data manually.</p>
+    <h2 class="text-2xl font-medium dark:text-white">Does it support all kinds of exif metadata?</h2>
+    <p class="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">Yes, almost all standard EXIF metadatas are supported</p>
 
     <h2 class="text-2xl font-medium dark:text-white">Any information stored in the browser?</h2>
     <p class="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">No information is stored in the browser once you close the browser or the tab.</p>
 
     <h2 class="text-2xl font-medium dark:text-white">How to contact us?</h2>
-    <p class="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">Email us to help@pdftableconvert.com</p>
+    <p class="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">Email us to exif@pdftableconvert.com</p>
   </main>
 </template>

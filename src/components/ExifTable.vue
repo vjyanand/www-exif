13a18b99783411908ba04a058f85ed90afc929d9
Vjy <vijay@iavian.com>
2024-10-15 12:55:59 +0530

    Add exif cleanup

--- src/components/ExifTable.vue
+++ src/components/ExifTable.vue
@@ -62,7 +62,7 @@ function delete_field(field) {
         <tr v-for="field in result['exif']" :key="field.key" :data-key="field.key" :data-type="field.typeName"
           :data-raw-value="field.value" class="odd:bg-white even:bg-gray-50">
 
-          <td class="py-3 px-6 text-center">
+          <td class="py-3 px-4 text-left">
             {{ field.label }}
             <span v-if="field.desc">ⓘ</span>
             <span v-if="field.desc" role="tooltip"
@@ -73,16 +73,16 @@ function delete_field(field) {
           </td>
 
           <template v-if="pageStore.editing_object && field.key === pageStore.editing_object['key']">
-            <td class="py-2 px-3 flex justify-center">
+            <td class="py-2 flex justify-start">
               <input class="h-8 placeholder:italic placeholder:text-slate-400 block bg-white w-5/6 text-start border border-slate-300 rounded-md shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm" type="text" :placeholder="field.value" v-model="new_value" />
             </td>
           </template>
           <template v-else>
-            <td class="py-3 text-center">{{ field.value }}</td>
+            <td class="py-3 text-start">{{ field.value }}</td>
           </template>
 
-          <td class="py-3 px-6 text-center">
-            <div v-if="!field.readonly" class="flex justify-center">
+          <td class="py-3 px-4">
+            <div v-if="!field.readonly" class="flex justify-end">
               <template v-if="pageStore.editing_object && field.key === pageStore.editing_object['key']">
 
                 <img width="22" v-if="pageStore.editing_object['value'] === new_value" class="bg-slate-400 mr-2"

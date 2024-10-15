972d7dacb6e2bc68ecc94ea90810be671275494e
Vjy <vijay@iavian.com>
2024-10-15 14:31:45 +0530

    Add exif cleanup

--- src/components/Slug.vue
+++ src/components/Slug.vue
@@ -3,9 +3,9 @@
     <h1 class="my-2 py-1 text-4xl font-extrabold leading-none sm:px-1 tracking-tight md:text-5xl lg:text-4xl">
       <span class="emo">Privacy first, Secure, Offline </span>Multimedia file metadata tool
     </h1>
-    <p class="my-2 text-lg font-normal text-gray-500 lg:text-xl sm:px-1">
+    <h2 class="my-2 text-lg font-normal text-gray-500 lg:text-xl sm:px-1">
       Enhanced privacy, most secure and offline Multimedia file metadata tool. <b>Your Files Stay on Your Device!</b>
-    </p>
+    </h2>
   </div>
 </template>
 <style scoped>
